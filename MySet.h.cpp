269763509a36264bf43061df1938ff125db2d3d2
DDHunter13 <1310nero@mail.ru>
2015-03-29 12:46:03 +0300

    Create MySet.h.cpp

---
+++ MySet.h.cpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ISet.h"
+
+template <class T> class MySet : public ISet < T > {
+private:
+	//Харнилище данных
+	T* data;
+	//текущий размер
+	int size1;
+	//Максимальная вместимость
+	int capacity;
+
+public:
+	//Конструктор
+	MySet(){
+		capacity = 10;
+		size1 = 0;
+		data = new T[capacity];
+	}
+
+	//Деструктор
+	~MySet(){
+		delete[] data;
+	}
+
+	// узнать размер множества    
+	virtual int size() const {
+		return size1 + 1;
+	}
+
+	// содержится ли элемент    
+	virtual bool contains(const T& elem) const {
+		for (int i = 0; i < size1 + 1; ++i) {
+			if (data[i] == elem) { return true; }
+		}
+		return false;
+	}
+
+	// удалить элемент    
+	virtual void remove(const T& elem) {	
+		for (int i = 0; i < size1 + 1; ++i) {
+			if (data[i] == elem) {
+				for (int j = i + 1; j < sizq1; ++j) {
+					data[j - 1] = data[j];
+				}
+				size1--;
+			}
+		}
+	}
+
+	// добавить элемент    
+	virtual void add(const T& elem) {
+		data[size1++] = elem;
+		//увеличение размера массива по мере заполнения
+		if (size1 == capacity - 1) {
+			capacity = capacity * 2;
+			T* data1 = new T[capacity];
+			for (int i = 0; i < size1 + 1; ++i) {
+				data1[i] = data[i];
+			}
+			std::swap(data1, data);
+			delete[] data1;
+		}
+	}
+};
